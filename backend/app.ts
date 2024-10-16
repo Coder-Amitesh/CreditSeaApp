8de15e071303194b77f78bba46b9f9c9d3dab68b
Coder-Amitesh <103002926+Coder-Amitesh@users.noreply.github.com>
2024-10-16 20:00:55 +0530

    Revert "Initial Commit"
    This reverts commit 078093b662ac2f796369c2b52f480bc7dc6c7e34.

--- backend/app.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import loanRoutes from './routes/loanRoutes';
-
-const app = express();
-
-app.use(express.json()); // For parsing application/json
-app.use(loanRoutes); // Use loan routes
-
-export default app;
